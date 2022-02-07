45f94177034fa33a006c275166bdf3f98af7ab63
Joerg Mueller <joergm@hey.com>
2022-02-07 16:02:38 +0100

    Assignment 4.1, 2

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-const TYPE_SYSTEM = "system";
-const TYPE_USER = "user";
+const TYPE_SYSTEM = 'system';
+const TYPE_USER = 'user';
 
-const createMessage = (textBody, type, sender) => ({textBody, type, sender, render: renderMessage});
+const createMessage = (textBody, type, sender) => ({
+  textBody,
+  type,
+  sender,
+  render: renderMessage,
+});
 
-const m1 = createMessage("Lisa enters the chat", TYPE_SYSTEM);
-const m2 = createMessage("Paul enters the chat", TYPE_SYSTEM);
-const m3 = createMessage("Hello!", TYPE_USER, "Paul");
-const m4 = createMessage("Hello Paul! How are you?", TYPE_USER, "Lisa");
-const m5 = createMessage("Hi Lisa, i'm fine, thanks. How are you?", TYPE_USER, "Paul");
+let messages = [
+  createMessage('Lisa enters the chat', TYPE_SYSTEM),
+  createMessage('Paul enters the chat', TYPE_SYSTEM),
+  createMessage('Hello!', TYPE_USER, 'Paul'),
+  createMessage('Hello Paul! How are you?', TYPE_USER, 'Lisa'),
+  createMessage("Hi Lisa, i'm fine, thanks. How are you?", TYPE_USER, 'Paul'),
+];
 
 function renderMessage() {
-    let formattedMessage = "";
+  let formattedMessage = '';
 
-    if (this.type === TYPE_SYSTEM) {
-        formattedMessage = "..." + this.textBody + "...";
-    } else {
-        formattedMessage = `${this.sender}: ${this.textBody}`;
-    }
+  if (this.type === TYPE_SYSTEM) {
+    formattedMessage = '...' + this.textBody + '...';
+  } else {
+    formattedMessage = `${this.sender}: ${this.textBody}`;
+  }
 
-    return formattedMessage;
+  return formattedMessage;
 }
 
 function sendMessage(message) {
-    console.log(message);
-    message.render && console.log(message.render());
+  console.log(message);
+  message.render && console.log(message.render());
 }
 
-sendMessage(m1);
-sendMessage(m2);
-sendMessage(m3);
-sendMessage(m4);
-sendMessage(m5);
+//of loop
+for (let message of messages) {
+  sendMessage(message);
+}
+
+//in loop
+for (let i in messages) {
+  sendMessage(messages[i]);
+}
+
+//forEach
+messages.forEach((m) => sendMessage(m));
+
+//forEach Shorthand
+messages.forEach(sendMessage);
