08cde03073aa94e8dee2dee6060c413ebf08a161
Joerg Mueller <joergm@hey.com>
2022-02-08 11:18:39 +0100

    Assignment 6

--- script.js
+++ script.js
@@ -1,66 +1,80 @@
-function Message(textBody) {
+class Message {
+  constructor(textBody) {
     this.textBody = textBody;
+  }
+
+  render() {
+    return `${this.textBody}`;
+  }
 }
 
-Message.prototype.render = function () {
-    return this.textBody;
-};
+class SystemMessage extends Message {
+  constructor(textBody) {
+    super(textBody);
+  }
 
-
-function SystemMessage(textBody) {
-    Message.call(this, textBody);
+  render() {
+    return `... ${this.textBody} ...`;
+  }
 }
 
-SystemMessage.prototype = Object.create(Message.prototype); // What happens when using "new Message() "instead?
-SystemMessage.prototype.constructor = SystemMessage;
-
-SystemMessage.prototype.render = function () {
-    return "..." + this.textBody + "...";
-};
-
-function UserMessage(textBody, sender) {
-    Message.call(this, textBody); // Why not just "Message(textBody)"?
+class UserMessage extends Message {
+  constructor(textBody, sender) {
+    super(textBody);
     this.sender = sender;
+  }
+  render() {
+    return `${this.sender}: ${this.textBody}`;
+  }
 }
 
-UserMessage.prototype = Object.create(Message.prototype);
-UserMessage.prototype.constructor = UserMessage;
-
-UserMessage.prototype.render = function () {
-    return `${this.sender}: ${this.textBody}`;
-};
-
-
-const messages = [
-    new SystemMessage("Lisa enters the chat"),
-    new SystemMessage("Paul enters the chat"),
-    new UserMessage("Hello!", "Paul"),
-    new UserMessage("Hello Paul! How are you?", "Lisa"),
-    new UserMessage("Hi Lisa, i'm fine, thanks. How are you?", "Paul")
+const initialMessages = [
+  new SystemMessage('Lisa enters the chat'),
+  new SystemMessage('Paul enters the chat'),
+  new UserMessage('Hello!', 'Paul'),
+  new UserMessage('Hello Paul! How are you?', 'Lisa'),
+  new UserMessage("Hi Lisa, i'm fine, thanks. How are you?", 'Paul'),
 ];
 
-function sendMessage(message) {
+class Chat {
+  constructor() {
+    this.messages = [];
+  }
+
+  sendMessage(message) {
+    this.messages.push(message);
     console.log(message);
     message.render && console.log(message.render());
+  }
+
+  get membersOfUserMsgs() {
+    return new Set(
+      this.messages
+        .map((message) => message.sender)
+        .filter((member) => member !== undefined)
+    );
+  }
+
+  get memberNames() {
+    return Array.from(this.membersOfUserMsgs);
+  }
+
+  get wordsPerMember() {
+    return this.messages.reduce((wordsPerMember, message) => {
+      const { sender, textBody } = message;
+      const wordCount = textBody.split(' ').length;
+      sender &&
+        (wordsPerMember[sender]
+          ? (wordsPerMember[sender] += wordCount)
+          : (wordsPerMember[sender] = wordCount));
+
+      return wordsPerMember;
+    }, {});
+  }
 }
 
-messages.forEach(message => sendMessage(message));
+let chat = new Chat();
 
-const membersOfUserMsgs = messages
-    .map(message => message.sender)
-    .filter(member => member !== undefined);
-
-const memberNames = Array.from(new Set(membersOfUserMsgs));
-console.log("Member names: ", memberNames);
-
-const wordsPerMember = messages.reduce((wordsPerMember, message) => {
-    const {sender, textBody} = message;
-    const wordCount = textBody.split(" ").length;
-    sender && (wordsPerMember[sender] ?
-        wordsPerMember[sender] += wordCount :
-        wordsPerMember[sender] = wordCount);
-
-    return wordsPerMember;
-}, {});
-
-console.log("Words per Member: ", wordsPerMember);
+initialMessages.forEach((message) => chat.sendMessage(message));
+console.log('Member names: ', chat.memberNames);
+console.log('Words per Member: ', chat.wordsPerMember);
